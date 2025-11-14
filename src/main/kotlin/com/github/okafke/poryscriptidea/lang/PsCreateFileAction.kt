9d0625e382672197e9aa47da009eaf58fdb0092f
okfk <65917827+okafke@users.noreply.github.com>
2025-11-14 16:27:33 +0100

    fix: add default extension for create file action

--- src/main/kotlin/com/github/okafke/poryscriptidea/lang/PsCreateFileAction.kt
+++ src/main/kotlin/com/github/okafke/poryscriptidea/lang/PsCreateFileAction.kt
@@ -3,5 +3,8 @@ package com.github.okafke.poryscriptidea.lang
 import com.intellij.ide.actions.CreateFileAction
 
 class PsCreateFileAction: CreateFileAction() {
+    override fun getDefaultExtension(): String {
+        return "pory"
+    }
 
 }
